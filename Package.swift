6fce6abf5179e4ab7958dd1b8cc4ac32cb0917fa
Sagar Dagdu <shags032@gmail.com>
2024-07-22 21:40:01 +0530

    Update Swift Package

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let package = Package(name: "SDDownloadManager",
-                      platforms: [.iOS(.v10)],
-                      products: [.library(name: "SDDownloadManager",
-                                          targets: ["SDDownloadManager"])],
-                      targets: [.target(name: "SDDownloadManager",
-                                        path: "SDDownloadManager/Classes", sources: ["SDDownloadManager/Classes"])])
+let package = Package(
+    name: "SDDownloadManager",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "SDDownloadManager", 
+            targets: ["SDDownloadManager"])],
+    targets: [.target(name: "SDDownloadManager",
+                      path: "SDDownloadManager/Classes")
+    ]
+)
